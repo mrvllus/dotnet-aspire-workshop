970870592b0c01e3f6302814a3031aff49f3f12f
James Montemagno <james.montemagno@gmail.com>
2025-03-10 08:56:45 -0700

    Add openapi

--- start/Api/Program.cs
+++ start/Api/Program.cs
@@ -1,9 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
 
 builder.Services.AddNwsManager();
 
