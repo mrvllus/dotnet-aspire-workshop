7da7d69a47f13a313de9ff384050ebd895fdea76
Tom Maravilla <tlmaravilla@churchofjesuschrist.org>
2025-08-29 10:30:07 -0600

    Add MyWeatherHub project to application builder
    Incorporated the `Projects.MyWeatherHub` project into the application builder as "website". This addition complements the existing `Projects.Api` project, enhancing the application's functionality.

--- start/AppHost/AppHost.cs
+++ start/AppHost/AppHost.cs
@@ -2,4 +2,9 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var api = builder.AddProject<Projects.Api>("api");
 
+
+var web = builder.AddProject<Projects.MyWeatherHub>("website");
+
+
+
 builder.Build().Run();
