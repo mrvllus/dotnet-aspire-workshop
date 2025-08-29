4f5ef98c9fbc62ff4c8a3d1b1ea3653e18a5bad0
Thomas Maravilla <89047133+mrvllus@users.noreply.github.com>
2025-08-29 11:41:19 -0600

    Enhance project integration and update configuration (#3)
    - Added reference to `api` project in `AppHost.cs` for `MyWeatherHub`.
    - Changed `WeatherEndpoint` to a hardcoded URL in `Program.cs`, removing reliance on configuration.
    - Removed `WeatherEndpoint` setting from `appsettings.json`.

--- start/AppHost/AppHost.cs
+++ start/AppHost/AppHost.cs
@@ -3,7 +3,8 @@ var builder = DistributedApplication.CreateBuilder(args);
 var api = builder.AddProject<Projects.Api>("api");
 
 
-var web = builder.AddProject<Projects.MyWeatherHub>("website");
+var web = builder.AddProject<Projects.MyWeatherHub>("website")
+    .WithReference(api);
 
 
 
