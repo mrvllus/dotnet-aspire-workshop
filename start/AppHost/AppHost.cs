60d4b85f0ff81f3437fb1bf2b8f829e8bfd77b13
Tom Maravilla <tlmaravilla@churchofjesuschrist.org>
2025-08-29 14:27:23 -0600

    Enhance Redis caching and project dependency management
    Updated `AppHost.cs` to include `WithRedisInsight()` for Redis caching. Introduced `WaitFor` method for Redis cache and API project to ensure readiness before proceeding. Improved dependency management for `MyWeatherHub` project by adding `WaitFor(api)`. These changes enhance the initialization sequence and reliability of application components.

--- start/AppHost/AppHost.cs
+++ start/AppHost/AppHost.cs
@@ -1,13 +1,15 @@
 var builder = DistributedApplication.CreateBuilder(args);
-var cache = builder.AddRedis("cache");
+
+var cache = builder.AddRedis("cache")
+    .WithRedisInsight();
 
 var api = builder.AddProject<Projects.Api>("api")
-    .WithReference(cache);
-
+    .WithReference(cache)
+    .WaitFor(cache);
 
 var web = builder.AddProject<Projects.MyWeatherHub>("website")
-    .WithReference(api);
-
-
+    .WithReference(api)
+    .WaitFor(api)
+    .WithExternalHttpEndpoints();
 
 builder.Build().Run();
